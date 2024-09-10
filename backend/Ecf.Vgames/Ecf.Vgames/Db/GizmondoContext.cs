567c5f76513d9e5f0501cc5c79b481fc7cd20d47
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-09-10 09:55:32 +0200

    update: created entities and dbcontext

---
+++ backend/Ecf.Vgames/Ecf.Vgames/Db/GizmondoContext.cs
@@ -0,0 +1,17 @@
+﻿using Ecf.Vgames.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecf.Vgames.Db
+{
+    public class GizmondoContext : DbContext
+    {
+        public DbSet<Gizmondo> Gizmondoes { get; set; }
+
+        public DbSet<Publisher> Publisher { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=db_ecf_vgames");
+        }
+    }
+}
---
+++ backend/Ecf.Vgames/Ecf.Vgames/Models/Gizmondo.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Ecf.Vgames.Models
+{
+    public class Gizmondo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public string? Dev { get; set; }
+
+        [Required]
+        public int PublisherId { get; set; }
+
+        [ForeignKey("PublisherId")]
+        [JsonIgnore]
+        public Publisher? Publisher { get; set; }
+    }
+}
